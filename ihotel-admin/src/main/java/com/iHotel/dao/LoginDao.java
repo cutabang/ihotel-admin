f1c2a510f1b36b974b9dac267e53fc3b5efcf8df
Khomeini <khomeini.usman@hellopay.com>
2015-11-23 11:33:46 +0700

    test1 commit for test2 branch

--- ihotel-admin/src/main/java/com/iHotel/dao/LoginDao.java
+++ ihotel-admin/src/main/java/com/iHotel/dao/LoginDao.java
@@ -3,5 +3,5 @@ package com.iHotel.dao;
 import com.iHotel.entity.AdminUser;
 
 public interface LoginDao {
-    AdminUser login(String userId, String password) throws Exception;
+    AdminUser login(String userId, String passwordss) throws Exception;
 }
